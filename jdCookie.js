082ada598e975ef390896b752fc25bdb33a8d927
spk122 <42504388+spk122@users.noreply.github.com>
2021-05-16 11:18:10 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  'pt_key=AAJgoFyPADDSwDtM1XfZNsQ2RMFWwwaslhBxfFAlBIpMaAv0oHoLgbgnnb-Jk2QojkW-RlFq1oo;pt_pin=jd_547e2eff14ad3',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJgoI6xADAEQRZMT1chB58SFWTKNVQPMLhw_SBIQhRFc7UgvepgE9NBdOtgFfxfPcuhQpe18NQ;pt_pin=jd_547e2eff14ad3',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
